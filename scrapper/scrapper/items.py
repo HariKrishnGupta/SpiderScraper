aff244fd054f8efb53e53362476e89948433878d
Hari Gupta <hkrishn22@gmail.com>
2015-01-22 23:13:30 -0800

    Scrapper implemented

--- scrapper/scrapper/items.py
+++ scrapper/scrapper/items.py
@@ -9,6 +9,22 @@ import scrapy
 
 
 class ScrapperItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()l
-    link = scrapy.Field()
+	# define the fields for your item here like:
+	# name = scrapy.Field()l
+	link = scrapy.Field()
+
+class MuseumItem(scrapy.Item):
+	museumNumber = scrapy.Field()
+	producerName = scrapy.Field()
+	description = scrapy.Field()
+	schoolOrStyle = scrapy.Field()
+	date = scrapy.Field()
+	materials = scrapy.Field()
+	technique = scrapy.Field()
+	height = scrapy.Field()
+	width = scrapy.Field()
+	curatorsComments = scrapy.Field()
+	location = scrapy.Field()
+	acquisitionDate = scrapy.Field()
+	department = scrapy.Field()
+	registrationNumber = scrapy.Field()
